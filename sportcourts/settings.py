57b71290e5956c62abb484889e114a042c90629b
Vitaliy Kharchenko <harchenko.grape@gmail.com>
2015-07-29 13:56:44 +0500

    deploychanges

--- sportcourts/settings.py
+++ sportcourts/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-IS_PRODUCTION = False
+IS_PRODUCTION = True
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
